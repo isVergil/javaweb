53c58478597c202b2947bedf8423252953ca02e6
bill <190384497@qq.com>
2020-12-10 13:56:41 +0800

    test

--- src/com/jdbc/JDBCTools.java
+++ src/com/jdbc/JDBCTools.java
@@ -14,6 +14,7 @@ public class JDBCTools {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
+
         }
     }
 
